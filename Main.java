f0a487c369d23d1e54912d7109ef816bd55f6af1
fiscn <fiscn2709@wrdsb.ca>
2020-09-14 23:43:22 +0000

    finished Question A1Q2 program

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;  
+
 /**
- *
- * @author 
+ *Will divide two numbers and provide a remainder 
+ * @author Neil Fischer  
  */
 public class Main {
 
@@ -10,6 +12,25 @@ public class Main {
    */
   public static void main(String[] args) {
     // TODO code application logic here
-    
+     Scanner input = new Scanner(System.in); 
+
+    // Ask the user to input two numbers to divide 
+    System.out.println("Please enter in two integers, on seperate lines, to divide.");
+
+    //Take the first number 
+    int X = input.nextInt(); 
+
+    //Take the second number 
+    int Y = input.nextInt(); 
+
+    //divide the numbers  
+    int A = X / Y; 
+
+    // tell the user the answer 
+    System.out.println(X + "/" + Y +  "is"  + A +  "with a remainder of" +  X%Y);
+
+ 
+
+
   }
 }
